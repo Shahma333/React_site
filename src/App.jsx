eb52fe87170048e04f201a26f5dcd4013857e91a
shahma <shahmak333@gmail.com>
2024-10-23 11:58:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import ShopPage from "./Pages/ShopPage"
+import { CartProvider } from "./Providers/CartProvider"
+import { CounterProvider } from "./Providers/CounterProvider"
+
+const App=()=>{
+  return  <CounterProvider>
+  <CartProvider>
+      <ShopPage />
+  </CartProvider>
+</CounterProvider>
+
+}
+export default App
---
+++ src/Pages/Context.jsx
@@ -0,0 +1,19 @@
+import { useCounter } from "../Providers/CounterProvider"
+
+
+const Context = () => {
+
+    const { counter, setCounter } = useCounter()
+
+    return (
+        <div className="d-flex align-items-center gap-3 mt-5 flex-column">
+            <div>{counter}</div>
+            <div className="d-flex gap-3 justify-content-center">
+                <button onClick={() => setCounter(counter + 1)} className="btn btn-primary rounded">Increment</button>
+                <button onClick={() => setCounter(counter - 1)} className="btn btn-danger rounded">Decrement</button>
+            </div>
+        </div>
+    )
+}
+
+export default Context
